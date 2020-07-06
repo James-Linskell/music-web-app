8dd7065a53436157a9132193356b92a350b71e18
jamie <jhdlinskell@protonmail.com>
2020-07-06 16:46:25 +0100

    FIXED ALIGNMENT ISSUE!!! And removed redundant class and import ResultsPage. Apparently it was an issue in CSS with both components having identical className. I would guess these are shared over multiple css files. Still doesn't make sense to me though.

--- src/routes/SearchPage.js
+++ src/routes/SearchPage.js
@@ -3,7 +3,6 @@ import hellify from '../hellify.png';
 import '../styles/SearchPage.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CardMaker from '../components/CardMaker';
-import ResultsPage from "../routes/ResultsPage";
 import FetchSearchData from '../components/FetchSearchData';
 
 class SearchPage extends React.Component {

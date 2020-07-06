84ec21d55504b736eef16827b8b2d0772c282abf
jamie <jhdlinskell@protonmail.com>
2020-07-06 14:52:00 +0100

    Fixed background scaling sizing issue.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import '../styles/SearchPage.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import HomePage from "./routes/HomePage";
 
-
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Route path="/" exact component={this.state.currentRoute}/>
+                <Route path="/home" component={HomePage}/>
+            </Router>
+        )
+    }
+}

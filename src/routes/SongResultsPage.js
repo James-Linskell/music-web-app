b3fecc516f33e20056c57968d58b49a2f38de228
jamie <jhdlinskell@protonmail.com>
2020-07-08 12:22:03 +0100

    Added grid dashboard display for data visualisation.

--- src/routes/SongResultsPage.js
+++ src/routes/SongResultsPage.js
@@ -3,12 +3,19 @@ import FetchTrackFeatures from "../components/FetchTrackFeatures";
 import "../styles/SongResultsPage.css";
 import { Bar } from 'react-chartjs-2';
 
+// https://www.youtube.com/watch?v=-qOe8lBAChE
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout
+
 // this.props.location.search gives the song id!!!
 class SongResultsPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            rawData: null,
+            data: [],
+            options: [],
+            prompt: null,
+            invalid: false
         }
     }
 
@@ -18,34 +25,95 @@ class SongResultsPage extends React.Component {
         // Error handling if no search results are returned:
         if (data.length === 0) {
             this.setState({
-                prompt: "No results found!",
-                results: <div className="Margin" ></div>
+                prompt: "Invalid song ID",
+                invalid: true
             });
             return;
         }
+        this.setState({rawData: data});
+    };
+
+    generateSongFeatures() {
         const chartData = {
             labels: ["Acousticness", "Dancability", "Energy", "Liveness", "Speechiness", "Valence"],
             datasets: [{
                 label: "Song features",
-                backgroundColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'midnightblue',
                 borderColor: 'rgb(255, 99, 132)',
-                data: [data.acousticness, data.danceability, data.energy, data.liveness, data.speechiness, data.valence]
+                data: [this.state.rawData.acousticness, this.state.rawData.danceability, this.state.rawData.energy,
+                    this.state.rawData.liveness, this.state.rawData.speechiness, this.state.rawData.valence]
             }]
         }
-        this.setState({data: chartData})
-        console.log(data);
-    };
+        const chartOptions = {
+            legend: {
+                display: "true",
+                labels: {
+                    fontColor: "white"
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: "white"
+                    },
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontColor: "white"
+                    },
+                }]
+            }
+        }
+        this.setState({
+            data: chartData,
+            options: chartOptions
+        })
+    }
 
-    componentDidMount() {
-        this.waitForFetch();
+    componentDidMount = async () =>  {
+        await this.waitForFetch();
+        this.generateSongFeatures();
     }
 
     render() {
         return (
             <div className="Main">
-                Song Features:
-                <div className="Chart">
-                    <Bar data={this.state.data}/>
+                Song Data:
+                <div className="Container">
+                    <div>
+                        <h2>Song Features</h2>
+                        <p className="Chart">
+                            <Bar data={this.state.data} options={this.state.options}/>
+                        </p>
+                    </div>
+                    <div>
+                        <h2>Sample 2</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 3</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 4</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 1</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 2</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 3</h2>
+                        <p></p>
+                    </div>
+                    <div>
+                        <h2>Sample 4</h2>
+                        <p></p>
+                    </div>
                 </div>
             </div>
         )

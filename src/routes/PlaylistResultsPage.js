622ea55f0a1e517795ee8d6fb9da9793f44dba76
jamie <jhdlinskell@protonmail.com>
2020-07-20 12:38:41 +0100

    Created histograms for mood features.

--- src/routes/PlaylistResultsPage.js
+++ src/routes/PlaylistResultsPage.js
@@ -5,9 +5,18 @@ import SongCard from "../components/SongCard";
 import '../styles/SongResultsPage.css'
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import {HorizontalBar} from "react-chartjs-2";
+import Histogram from "../components/Histogram";
 
 
 class PlaylistResultsPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            danceHist: null,
+            energyHist: null,
+            valenceHist: null
+        }
+    }
 
     componentDidMount() {
         this.waitFortracks();
@@ -48,12 +57,189 @@ class PlaylistResultsPage extends React.Component {
     };
 
     simplifyData = async (data) => {
-        let dance = [];
-        let energy = [];
-        let valence = [];
+        let dance = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let energy = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let valence = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let danceArray = [];
+        let energyArray = [];
+        let valenceArray = [];
+        let danceIndex = 0;
+        let energyIndex = 0;
+        let valenceIndex = 0;
+        let n = 0;
         data.audio_features.forEach(track => {
-           dance.push(track.danceability)
+            if (track.danceability < 0.05) {
+                dance[0]++;
+            } else if (0.05 < track.danceability < 0.1) {
+                dance[1]++;
+            } else if (0.1 < track.danceability < 0.15) {
+                dance[2]++;
+            } else if (0.15 < track.danceability < 0.2) {
+                dance[3]++;
+            } else if (0.2 < track.danceability < 0.25) {
+                dance[4]++;
+            } else if (0.25 < track.danceability < 0.3) {
+                dance[5]++;
+            } else if (0.3 < track.danceability < 0.35) {
+                dance[6]++;
+            } else if (0.35 < track.danceability < 0.4) {
+                dance[7]++;
+            } else if (0.4 < track.danceability < 0.45) {
+                dance[8]++;
+            } else if (0.45 < track.danceability < 0.55) {
+                dance[9]++;
+            } else if (0.5 < track.danceability < 0.55) {
+                dance[10]++;
+            } else if (0.55 < track.danceability < 0.6) {
+                dance[11]++;
+            } else if (0.6 < track.danceability < 0.65) {
+                dance[12]++;
+            } else if (0.65 < track.danceability < 0.7) {
+                dance[13]++;
+            } else if (0.7 < track.danceability < 0.75) {
+                dance[14]++;
+            } else if (0.75 < track.danceability < 0.8) {
+                dance[15]++;
+            } else if (0.8 < track.danceability < 0.85) {
+                dance[16]++;
+            } else if (0.85 < track.danceability < 0.9) {
+                dance[17]++;
+            } else if (0.9 < track.danceability < 0.95) {
+                dance[18]++;
+            } else if (0.95 < track.danceability < 1.0) {
+                dance[19]++;
+            }
+
+            if (track.energy < 0.05) {
+                energy[0]++;
+            } else if (0.05 < track.energy < 0.1) {
+                energy[1]++;
+            } else if (0.1 < track.energy < 0.15) {
+                energy[2]++;
+            } else if (0.15 < track.energy < 0.2) {
+                energy[3]++;
+            } else if (0.2 < track.energy < 0.25) {
+                energy[4]++;
+            } else if (0.25 < track.energy < 0.3) {
+                energy[5]++;
+            } else if (0.3 < track.energy < 0.35) {
+                energy[6]++;
+            } else if (0.35 < track.energy < 0.4) {
+                energy[7]++;
+            } else if (0.4 < track.energy < 0.45) {
+                energy[8]++;
+            } else if (0.45 < track.energy < 0.55) {
+                energy[9]++;
+            } else if (0.5 < track.energy < 0.55) {
+                energy[10]++;
+            } else if (0.55 < track.energy < 0.6) {
+                energy[11]++;
+            } else if (0.6 < track.energy < 0.65) {
+                energy[12]++;
+            } else if (0.65 < track.energy < 0.7) {
+                energy[13]++;
+            } else if (0.7 < track.energy < 0.75) {
+                energy[14]++;
+            } else if (0.75 < track.energy < 0.8) {
+                energy[15]++;
+            } else if (0.8 < track.energy < 0.85) {
+                energy[16]++;
+            } else if (0.85 < track.energy < 0.9) {
+                energy[17]++;
+            } else if (0.9 < track.energy < 0.95) {
+                energy[18]++;
+            } else if (0.95 < track.energy < 1.0) {
+                energy[19]++;
+            }
+
+            if (track.valence < 0.05) {
+                valence[0]++;
+            } else if (0.05 < track.valence < 0.1) {
+                valence[1]++;
+            } else if (0.1 < track.valence < 0.15) {
+                valence[2]++;
+            } else if (0.15 < track.valence < 0.2) {
+                valence[3]++;
+            } else if (0.2 < track.valence < 0.25) {
+                valence[4]++;
+            } else if (0.25 < track.valence < 0.3) {
+                valence[5]++;
+            } else if (0.3 < track.valence < 0.35) {
+                valence[6]++;
+            } else if (0.35 < track.valence < 0.4) {
+                valence[7]++;
+            } else if (0.4 < track.valence < 0.45) {
+                valence[8]++;
+            } else if (0.45 < track.valence < 0.55) {
+                valence[9]++;
+            } else if (0.5 < track.valence < 0.55) {
+                valence[10]++;
+            } else if (0.55 < track.valence < 0.6) {
+                valence[11]++;
+            } else if (0.6 < track.valence < 0.65) {
+                valence[12]++;
+            } else if (0.65 < track.valence < 0.7) {
+                valence[13]++;
+            } else if (0.7 < track.valence < 0.75) {
+                valence[14]++;
+            } else if (0.75 < track.valence < 0.8) {
+                valence[15]++;
+            } else if (0.8 < track.valence < 0.85) {
+                valence[16]++;
+            } else if (0.85 < track.valence < 0.9) {
+                valence[17]++;
+            } else if (0.9 < track.valence < 0.95) {
+                valence[18]++;
+            } else if (0.95 < track.valence < 1.0) {
+                valence[19]++;
+            }
+
+            // Find index of comparison song:
+            if (n === 0) {
+                danceIndex = dance.findIndex((val) => {
+                    return val >= 0.01;
+                });
+                energyIndex = energy.findIndex((val) => {
+                    return val >= 0.01;
+                });valenceIndex = valence.findIndex((val) => {
+                    return val >= 0.01;
+                });
+            }
+
+            danceArray.push(track.danceability);
+            energyArray.push(track.energy);
+            valenceArray.push(track.valence);
+            n++;
         });
+
+        const danceMean = danceArray.reduce((a,b) => a+b)/n;
+        const danceStdev = Math.sqrt(danceArray.map(x => Math.pow(x-danceMean,2)).reduce((a,b) => a+b)/n);
+        console.log("Standard deviation: " + danceStdev);
+        console.log("Mean: " + danceMean);
+        console.log("Value: " + data.audio_features[0].danceability);
+        const energyMean = energyArray.reduce((a,b) => a+b)/n;
+        const energyStdev = Math.sqrt(energyArray.map(x => Math.pow(x-energyMean,2)).reduce((a,b) => a+b)/n);
+        const valenceMean = danceArray.reduce((a,b) => a+b)/n;
+        const valenceStdev = Math.sqrt(danceArray.map(x => Math.pow(x-valenceMean,2)).reduce((a,b) => a+b)/n);
+        
+        let danceVal = data.audio_features[0].danceability;
+        let energyVal = data.audio_features[0].energy;
+        let valenceVal = data.audio_features[0].danceability;
+        console.log(danceVal);
+
+        if ((danceMean - danceStdev) <= danceVal && danceVal <= (danceMean + danceStdev)) {
+            console.log("Acurracy: 1 sigma");
+        } else if ((danceMean - (2 * danceStdev)) < danceVal && danceVal < (danceMean + (2 * danceStdev))) {
+            console.log("Acurracy: 2 sigma");
+        } else if ((danceMean - (3 * danceStdev)) < danceVal && danceVal < (danceMean + (3 * danceStdev))) {
+            console.log("Acurracy: 3 sigma");
+        }
+
+        this.setState({
+            danceHist: <Histogram data={dance} songIndex={danceIndex}/>,
+            energyHist: <Histogram data={energy} songIndex={energyIndex}/>,
+            valenceHist: <Histogram data={valence} songIndex={valenceIndex}/>
+        })
     }
 
     //CHANGE: get new audio data every time, or the user can't bookmark this page ///////////////////////////////////////////
@@ -80,9 +266,14 @@ class PlaylistResultsPage extends React.Component {
                         <h2>Song Mood Features:<button style={{display: "flex", marginLeft: "2vw"}}>What's this?</button></h2>
                         <hr/>
                         <p>
-
+                            <div className='myDiv'/>
                         </p>
+                        <div style={{ display: 'flex', maxWidth: 900 }}>
+                        </div>
                     </div>
+                </div>
+                <div style={{maxWidth: "50vw"}}>
+                    {this.state.danceHist}{this.state.energyHist}{this.state.valenceHist}
                 </div>
             </div>
         )
